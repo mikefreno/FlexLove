754894126fffb8aa084903408e8bcb5ce2102f0f
Michael Freno <michaelt.freno@gmail.com>
2025-11-13 10:27:02 -0500

    fixe

--- FlexLove.lua
+++ FlexLove.lua
@@ -21,44 +21,36 @@ local Color = req("Color")
 local Theme = req("Theme")
 local enums = utils.enums
 
--- ====================
--- FlexLove - UI Library
--- ====================
-
 ---@class FlexLove
-local flexlove = {
-  _VERSION = "FlexLove v0.1.0",
-  _DESCRIPTION = "UI Library for LÖVE Framework based on flexbox",
-  _URL = "https://github.com/mikefreno/FlexLove",
-  _LICENSE = [[
-    MIT License
+local flexlove = GuiState
 
-    Copyright (c) 2025 Mike Freno
+-- Add version and metadata
+flexlove._VERSION = "FlexLove v0.1.0"
+flexlove._DESCRIPTION = "UI Library for LÖVE Framework based on flexbox"
+flexlove._URL = "https://github.com/mikefreno/FlexLove"
+flexlove._LICENSE = [[
+  MIT License
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
+  Copyright (c) 2025 Mike Freno
 
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-  ]],
-}
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
 
--- Copy GuiState properties into flexlove
-for k, v in pairs(GuiState) do
-  flexlove[k] = v
-end
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+]]
 
 --- Initialize FlexLove with configuration
 ---@param config {baseScale?: {width?:number, height?:number}, theme?: string|ThemeDefinition, immediateMode?: boolean, stateRetentionFrames?: number, maxStateEntries?: number, autoFrameManagement?: boolean}

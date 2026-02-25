f1fae855951f7f7af68320986a82246f428fa2d2
Michael Freno <michaelt.freno@gmail.com>
2026-02-25 00:29:36 -0500

    v0.9.2 release

--- FlexLove.lua
+++ FlexLove.lua
@@ -63,7 +63,7 @@ local enums = utils.enums
 
 ---@class FlexLove
 local flexlove = Context
-flexlove._VERSION = "0.9.1"
+flexlove._VERSION = "0.9.2"
 flexlove._DESCRIPTION = "UI Library for LÖVE Framework based on flexbox"
 flexlove._URL = "https://github.com/mikefreno/FlexLove"
 flexlove._LICENSE = [[

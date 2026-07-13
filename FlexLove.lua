ca69ad4b4a268e674a75d710ebfcea8df45ec77b
Michael Freno <michaelt.freno@gmail.com>
2026-07-13 17:07:30 -0400

    v0.15.0 release

--- FlexLove.lua
+++ FlexLove.lua
@@ -136,7 +136,7 @@ local Transform = Animation and Animation.Transform or nil
 local enums = utils.enums
 
 local flexlove = Context
-flexlove._VERSION = "0.14.0"
+flexlove._VERSION = "0.15.0"
 flexlove._DESCRIPTION = "UI Library for LÖVE Framework based on flexbox"
 flexlove._URL = "https://github.com/mikefreno/FlexLove"
 flexlove._LICENSE = [[
